0f9e72c68f60dff37815c3e92873e33ba534ee19
Alyssa McDevitt <amcdevitt97@gmail.com>
2020-02-14 12:10:14 -0500

    converted timepicker from 24 to 12 hr format

--- src/screens/ScheduleScreen/scheduleScreen.js
+++ src/screens/ScheduleScreen/scheduleScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './styles';
 import CalendarPicker from 'react-native-calendar-picker';
 import TimePicker from "react-native-24h-timepicker";
+import Picker from 'react-native-picker';
 import moment from 'moment';
 
 class ScheduleScreen extends React.Component {
@@ -15,24 +16,73 @@ class ScheduleScreen extends React.Component {
         this.onDateChange = this.onDateChange.bind(this);
     }
 
-    onDateChange(date, hour, minute) {
-        console.log(hour);
+    onDateChange(date, hour, minute, meridiem) {
         if(hour != null && minute != null){
             date = moment(date);
+            // Convert to 24-hr format
+            if(meridiem == "PM"){
+                if(hour >=1 || hour<=11 ){
+                    console.log(hour);
+                    hour+=12
+                }
+                else if(hour==12){
+                    hour = 0;
+                }
+            }
             date.hour(hour);
             date.minute(minute);
-            this.TimePicker.close();
         }
         this.setState({
             selectedStartDate: date,
         });
-
     }
 
     onCancel() {
         this.TimePicker.close();
     }
 
+
+    _showTimePicker() {
+        let hours = [],
+            minutes = [];
+        for(let i=1;i<13;i++){
+            hours.push(i);
+        }
+        for(let i=1;i<61;i++){
+            minutes.push(i);
+        }
+        let pickerData = [hours, minutes, ['AM', 'PM']];
+
+        Picker.init({
+            pickerData,
+            pickerTitleText: 'Select Time',
+            wheelFlex: [1, 1, 2],
+            onPickerConfirm: pickedValue => {
+                console.log('area', pickedValue);
+                var minute = parseInt(pickedValue[1]);
+                var hour = parseInt(pickedValue[0]);
+                this.onDateChange(this.state.selectedStartDate, hour, minute, pickedValue[2]);
+            },
+            onPickerCancel: pickedValue => {
+                console.log('area', pickedValue);
+            },
+            onPickerSelect: pickedValue => {
+                let targetValue = [...pickedValue];
+                // forbidden some value such as some 2.29, 4.31, 6.31...
+                if(JSON.stringify(targetValue) !== JSON.stringify(pickedValue)){
+                    // android will return String all the time，but we put Number into picker at first
+                    // so we need to convert them to Number again
+                    targetValue.map((v, k) => {
+                        if(k !== 3){
+                            targetValue[k] = parseInt(v);
+                        }
+                    });
+                    Picker.select(targetValue);
+                }
+            }
+        });
+        Picker.show();
+    }
 
 
 
@@ -66,10 +116,8 @@ class ScheduleScreen extends React.Component {
 
                   </View>
 
-                  <TouchableHighlight
-                      onPress={() => this.TimePicker.open()}
-                      style={styles.button}
-                  >
+
+                  <TouchableHighlight style={styles.button} onPress={this._showTimePicker.bind(this)}>
                       <Text style={styles.buttonText}>Select a time</Text>
                   </TouchableHighlight>
 
